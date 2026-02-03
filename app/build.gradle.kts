9533dfdb98e1dbc9112d41f2044f57b98b261892
MyDataIntelligence <ferron@mydataintelligence.com>
2026-02-03 22:44:15 +0100

    Back to minSdk 26 for Android 16, v1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,15 +9,18 @@ android {
 
     defaultConfig {
         applicationId = "com.mymate.auto"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
+        }
+        debug {
+            isDebuggable = true
         }
     }
     compileOptions {
